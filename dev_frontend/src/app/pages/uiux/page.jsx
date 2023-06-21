ecc3c39bdcc9976ee3c9b3128307ee0b51c7abfb
himanshuchaudhary1110 <himanshu.chaudhary@devriser.com>
2023-06-21 15:36:22 +0530

    added bg to  uiux

---
+++ dev_frontend/src/app/pages/uiux/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useEffect } from "react";
+import "./uiux.css";
+
+export default function Uiux() {
+  return <div className="main-container">
+    <div className="bg-eff-1">
+      <div className="bg-eff-2"></div>
+    </div>
+  </div>;
+}
