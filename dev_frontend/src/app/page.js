30d62fe61564f64017c207321c7cadfbe90bdeab
himanshuchaudhary1110 <himanshu.chaudhary@devriser.com>
2023-06-21 11:01:38 +0530

    Changes Images

--- dev_frontend/src/app/page.js
+++ dev_frontend/src/app/page.js
@@ -1,6 +1,3 @@
-import { Why1 } from "assets/illustration/whyOurService";
-import { Box } from "../utils/custom";
-import { Icon2 } from "assets/icons";
 
 export default function Home() {
   return (
